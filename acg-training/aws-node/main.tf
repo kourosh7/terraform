3784d972b1bec848c265c2b320f5366a26e0b740
kourosh7 <kouroshm@gmail.com>
2023-12-30 22:51:16 -0800

    add acg-training

---
+++ acg-training/aws-node/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami = "ami-03e0029f86a2c74c3"
+  subnet_id = "subnet-bc3e12e7"
+  instance_type = "t3.medium"
+  tags = {
+    Name = "kourosh-ec2-instance"
+    DoNotDelete = "true"
+  }
+}
+
